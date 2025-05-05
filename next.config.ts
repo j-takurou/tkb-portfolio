6dad9090b987dc87015359ea610497da8f8588dd
j-takurou <nyk523jt@gmail.com>
2025-05-06 00:06:29 +0900

    サンプルデータ表示可能に、cssを修正し、常に２列表示に

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/portfolio',
+  basePath: '/.',
   assetPrefix: '/.',
   images: { unoptimized: true },
   trailingSlash: true,
